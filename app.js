21b7c9939b724de5fe37bebbbc865c4a85e629b1
Kevin Leung <keleung@microsoft.com>
2017-12-07 15:37:53 -0600

    initial push

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+ * Proud owner of kevin-leung NPM package. All rights reserved.
+ */
+
+// Import required engineers
+import RyanVolum from 'ryan-volum'
+import ChrisMayo from 'chris-mayo'
+import BillBarnes from 'bill-barnes'
+
+// Do something not so impressive
+const kevinLeung = () => {
+  return ChrisMayo() + 'is okay.'
+}
+
+export default kevinLeung
