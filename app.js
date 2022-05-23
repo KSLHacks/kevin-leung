185adbac38eebb30bfa80be6551dac511058d43a
Kevin Leung <leung.kevin23@gmail.com>
2022-05-22 20:46:26 -0400

    updates

--- app.js
+++ app.js
@@ -2,14 +2,9 @@
  * Proud owner of kevin-leung NPM package. All rights reserved.
  */
 
-// Import required engineers
-import RyanVolum from 'ryan-volum'
-import ChrisMayo from 'chris-mayo'
-import BillBarnes from 'bill-barnes'
-
 // Do something not so impressive
 const kevinLeung = () => {
-  return ChrisMayo() + 'is okay.'
+  return 'Kevin was here.'
 }
 
 export default kevinLeung
